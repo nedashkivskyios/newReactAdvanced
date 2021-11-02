9b2a1b0f8c566a89429b66d5e1d29869f4cf3ecd
nedashkivskyios <nedashkivskyios@gmail.com>
2021-11-02 21:08:35 +0200

    2

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  console.log('App rendering')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppTitle />
+      <Rating/>
+      <Accordion/>
     </div>
   );
+}
+
+const AppTitle = () => {
+  console.log('AppTitle rendering')
+  return (
+    <>This is APP Component</>
+  )
+}
+
+const Rating = () => {
+  console.log('Rating rendering')
+  return (
+    <div>
+      Rating
+      <Star/>
+      <Star/>
+      <Star/>
+      <Star/>
+      <Star/>
+    </div>
+  )
+}
+
+const Star = () => {
+  console.log('Star rendering')
+  return (
+    <div>star</div>
+  )
+}
+
+const Accordion = () => {
+  console.log('Accordion rendering')
+  return (
+    <div>
+      <h3>Accordion</h3>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  )
 }
 
 export default App;
